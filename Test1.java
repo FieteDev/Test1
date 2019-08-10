4e921f3c23e84fff15a6f4c84e6034c26c145137
Fieder Piening <frieder@piening.info>
2019-08-10 16:43:41 +0200

    Read name from command line

--- Test1.java
+++ Test1.java
@@ -1,5 +1,10 @@
 public class Test1 {
     public static void main(String[] args) {
-      System.out.println("Hello World");
+        if (args.length > 0) {
+            System.out.println("Hallo " + args[0] + "!");
+        }
+        else {
+            System.out.println("Hallo Du!");
+        }
     }
   }
